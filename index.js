6a275272581562c356e6ded8afd846a00a85af00
amostanls <tanamos@live.com>
2024-12-06 20:08:46 +0800

    amos: post interview

--- index.js
+++ index.js
@@ -1,11 +1,62 @@
 // expecting time to be a string in the format like '8:15' or '12:30'
 function convertTimeToWords(time) {
-  // TODO: real code goes here!
   if (time === '0:00') {
     return 'midnight';
+  } else if (time === '12:00'){
+    return 'midday';
   }
 
-  return 'half past eight';
+  let numberToWords = [
+    'zero',
+    'one',
+    'two',
+    'three',
+    'four',
+    'five',
+    'six',
+    'seven',
+    'eight',
+    'nine',
+    'ten',
+    'eleven',
+    'twelve',
+    'thirteen',
+    'fourteen',
+    'quarter',
+    'sixteen',
+    'seventeen',
+    'eighteen',
+    'nineteen',
+    'twenty',
+    'twenty one',
+    'twenty two',
+    'twenty three',
+    'twenty four',
+    'twenty five',
+    'twenty six',
+    'twenty seven',
+    'twenty eight',
+    'twenty nine',
+    'half',
+  ];
+
+  let tmp = time.split(":");
+  let hour = Number(tmp[0]);
+  let minute = Number(tmp[1]);
+
+  hour %= 12;
+  if(hour == 0) hour = 12;
+  let nextHour = hour + 1;
+  if(nextHour == 13) nextHour = 1;
+
+  if(minute == 0){
+    return `${numberToWords[hour]} o'clock`;
+  }
+  let result = (minute <= 30 ? 
+    `${numberToWords[minute]} past ${numberToWords[hour]}` : 
+    `${numberToWords[60 - minute]} to ${numberToWords[nextHour]}`
+  );
+  return result;
 }
 
 module.exports = { convertTimeToWords };
--- index.test.js
+++ index.test.js
@@ -6,6 +6,35 @@ describe('Time to words', () => {
     expect(timeInWords).toBe('midnight');
   });
 
+  it('Handles midday', () => {
+    const timeInWords = convertTimeToWords('12:00');
+    expect(timeInWords).toBe('midday');
+  });
+
+  it('handles o clock', () => {
+    const timeInWords = convertTimeToWords('2:00');
+    expect(timeInWords).toBe("two o'clock");
+  });
+
+  it('before 30 mins', () => {
+    const times = ['2:02', '2:05', '2:07', '2:10', '2:13'];
+    const expectedTimes = ["two past two", "five past two", "seven past two", "ten past two", "thirteen past two"];
+    for(let index = 0; index < times.length; index++){
+      const timeInWords = convertTimeToWords(times[index]);
+      expect(timeInWords).toBe(expectedTimes[index]);
+    }
+
+    
+  });
+
+  test.each([
+    {time: '13:40', expected: 'twenty to two'},
+    {time: '11:50', expected: 'ten to twelve'},
+    {time: '12:43', expected: 'seventeen to one'},
+  ])('handles ($time)', ({time, expected}) => {
+    expect(convertTimeToWords(time)).toBe(expected);
+  });
+
   it('Handles 30 - 8:30', () => {
     const timeInWords = convertTimeToWords('8:30');
     expect(timeInWords).toBe('half past eight');
@@ -15,4 +44,5 @@ describe('Time to words', () => {
     const timeInWords = convertTimeToWords('2:45');
     expect(timeInWords).toBe('quarter to three');
   });
+
 });
